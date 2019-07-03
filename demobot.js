6b72d361cb2c38887a5b2c8bd15aef56e00edf7f
ubelhj <ubelhj@uw.edu>
2019-07-03 16:19:42 -0700

    adds highscores

--- demobot.js
+++ demobot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const highscores = require("./highscores.json");
 const fs = require('fs');
 
 client.on("ready", () => {
@@ -14,7 +15,7 @@ client.on("message", message => {
     // This is the best way to define args. Trust me.
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     console.log(args);
-    if (args.length < 5) {
+    if (args.length < 5 || args.length > 8) {
         message.author.DMchannel.send("Try updating your stats with the following format: D: # of demos " +
             "E: # of exterminations U: Your Username");
     } else {
@@ -22,11 +23,32 @@ client.on("message", message => {
             message.author.DMchannel.send("Try updating your stats with the following format: D: # of demos " +
                 "E: # of exterminations U: Your Username");
             console.log("failed");
+            return;
         }
-        var content = "\n" + args[4] + "," + args[0] + "," + args[2];
-        // var leaderboard = fs.read("leaderboard.csv");
+        if (args[0] > highscores.manualDemoLimit) {
+            message.author.DMchannel.send("Your Demos are too high, and must be verified. Please DM JerryTheBee");
+            return;
+        }
+        if (args[2] > highscores.manualExtermLimit) {
+            message.author.DMchannel.send("Your Exterminations are too high, " +
+                "and must be verified. Please DM JerryTheBee");
+            return;
+        }
+
+        var name = "";
+
+        if (args.length > 5) {
+            for (int i = 4; i < args.length; i++) {
+                name = name + args[i];
+            }
+        } else {
+            name = args[4];
+        }
+
+        var content = "\n" + name + "," + args[0] + "," + args[2];
 
         fs.appendFile("leaderboard.csv", content);
+        message.author.DMchannel.send("Logged Leaderboard Data");
     }
 });
 
