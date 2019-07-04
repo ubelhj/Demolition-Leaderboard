98804af96e95626e499c9ad7542a8111f97da5fb
ubelhj <ubelhj@uw.edu>
2019-07-03 17:40:30 -0700

    tests with leaderboard as json

--- demobot.js
+++ demobot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const leaderboard = require("./leaderboard.json");
 const highscores = require("./highscores.json");
 const fs = require('fs');
 
@@ -17,18 +18,18 @@ client.on("message", message => {
     console.log(args);
     if (args.length < 4 || args.length > 8) {
         message.channel.send("Try updating your stats with the following format: D: # of demos " +
-            "E: # of exterminations U: Your Username");
+            "E: # of exterminations Your Username");
     } else if (isNaN(args[0]) || isNaN(args[2])) {
         message.channel.send("Try updating your stats with the following format: D: # of demos " +
-            "E: # of exterminations U: Your Username");
+            "E: # of exterminations Your Username");
     } else if (parseInt(args[0], 10) > parseInt(highscores.manualDemoLimit, 10)) {
         message.channel.send("Your Demos are in the top 20, and must be verified. Please DM JerryTheBee");
     } else if (parseInt(args[2], 10) > parseInt(highscores.manualExtermLimit, 10)) {
         message.channel.send("Your Exterminations are in the top 20, " +
              "and must be verified. Please DM JerryTheBee");
     } else {
+        // Sets name variable for long names
         var name = "";
-
         if (args.length > 4) {
             for (var i = 3; i < args.length; i++) {
                 name = name + " " + args[i];
@@ -37,10 +38,37 @@ client.on("message", message => {
             name = args[3];
         }
 
+        console.log(leaderboard.name);
+
+        // fs.readFile('./leaderboard.json', function (err, data) {
+        //     var parsedBoard = JSON.parse(data);
+        //
+        //     var author = message.author.id;
+        //     if (!parsedBoard.name) {
+        //         parsedBoard.push
+        //     }
+        //     if (!parsedBoard.name.Discord) {
+        //         parsedBoard.name.Discord = author;
+        //     }
+        //     if (parsedBoard.name.Discord.equals(author)) {
+        //         parsedBoard.name.Demos = args[0];
+        //         parsedBoard.name.Exterminations = args[2];
+        //     } else {
+        //         message.channel.send("Cannot update leaderboard for other users, " +
+        //             "Please DM JerryTheBee if something is wrong");
+        //     }
+        //
+        //     fs.writeFile("./leaderboard.json", JSON.stringify(parsedBoard), function (err) {
+        //         if (err) return console.log(err);
+        //         console.log(JSON.stringify(parsedBoard));
+        //     });
+        // });
+
         var content = "\n" + name + "," + args[0] + "," + args[2];
 
         fs.appendFile("leaderboard.csv", content);
         // //dmChannel.send("Logged Leaderboard Data");
+
     }
 });
 
