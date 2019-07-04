c061711dc37655514a330a6035ddd55f62987508
ubelhj <ubelhj@uw.edu>
2019-07-03 19:24:49 -0700

    Sets Car to authorized

--- demobot.js
+++ demobot.js
@@ -22,16 +22,6 @@ client.on("message", message => {
     } else if (isNaN(args[0]) || isNaN(args[2])) {
         message.channel.send("Try updating your stats with the following format: D: # of demos " +
             "E: # of exterminations Your Username\n Ex: D: 200 E: 10 Demo Leaderboard");
-    } else if (parseInt(args[0], 10) > parseInt(highscores.manualDemoLimit, 10)) {
-        message.channel.send("Congratulations, your stats qualify for a top 20 position! " +
-            "(A top 20 submission requires manual review from an admin and consequently may take longer to be " +
-            "accepted). A screenshot may be requested if your submission is suspect or results in a significant " +
-            "change in position. If you have any questions, please contact JerryTheBee");
-    } else if (parseInt(args[2], 10) > parseInt(highscores.manualExtermLimit, 10)) {
-        message.channel.send("Congratulations, your stats qualify for a top 20 position! " +
-            "(A top 20 submission requires manual review from an admin and consequently may take longer to be " +
-            "accepted). A screenshot may be requested if your submission is suspect or results in a significant " +
-            "change in position. If you have any questions, please contact JerryTheBee");
     } else {
         // Sets name variable for long names
         var name = "";
@@ -43,9 +33,8 @@ client.on("message", message => {
             name = args[3];
         }
 
-        var leaderboard = leaderboardJson.Leaderboard;
+        var leaderboard = leaderboardJson;
 
-        console.log(leaderboard.Car);
         console.log(leaderboard.hasOwnProperty(name));
         console.log(leaderboard[name]);
 
@@ -58,6 +47,19 @@ client.on("message", message => {
             leaderboard[name].Discord = author;
         }
         if (leaderboard[name].Discord == author) {
+            if (leaderboard[name].Authorized == 0) {
+                if (parseInt(args[0], 10) > parseInt(highscores.manualDemoLimit, 10)) {
+                    message.channel.send("Congratulations, your stats qualify for a top 20 position! " +
+                        "(A top 20 submission requires manual review from an admin and consequently may take longer to be " +
+                        "accepted). A screenshot may be requested if your submission is suspect or results in a significant " +
+                        "change in position. If you have any questions, please contact JerryTheBee");
+                } else if (parseInt(args[2], 10) > parseInt(highscores.manualExtermLimit, 10)) {
+                    message.channel.send("Congratulations, your stats qualify for a top 20 position! " +
+                        "(A top 20 submission requires manual review from an admin and consequently may take longer to be " +
+                        "accepted). A screenshot may be requested if your submission is suspect or results in a significant " +
+                        "change in position. If you have any questions, please contact JerryTheBee");
+                }
+            }
             leaderboard[name].Demos = args[0];
             leaderboard[name].Exterminations = args[2];
         } else {
