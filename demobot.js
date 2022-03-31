ce692a78301227ae7cd5e43d215edb1e8e197c5e
ubelhj <ubelhj@uw.edu>
2022-03-31 00:32:51 -0700

    Massive reorganizing of data

--- demobot.js
+++ demobot.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
-//const config = require("./config.json"); // For local Testing only
-const config = process.env; // for heroku usage
+const config = require("./config.json"); // For local Testing only
+//const config = process.env; // for heroku usage
 
 // hold jsons
 let leaderboard;
-let idmap;
 let highscores;
 
 // holds discord IDs of authorized moderators
@@ -15,7 +14,6 @@ const modRoleID = '431269016322048001';
 const fs = require('fs');
 const fetch = require('isomorphic-fetch');
 const Dropbox = require('dropbox').Dropbox;
-//const Jimp = require("jimp");
 let dbx = new Dropbox({accessToken: config.dropToken, fetch: fetch});
 let failedDownload = false;
 
@@ -122,19 +120,23 @@ client.on("messageCreate", async message => {
     // Ensures the message starts with the prefix "D:"
     if (message.content.toUpperCase().indexOf(config.prefix) !== 0) return;
 
-    message.reply("Leaderboard down for maintenance");
-    return;
-
     // if the previous download failed, tries again
     if (failedDownload) {
         download();
-    
     }
 
     // if two in a row have failed, gives up and warns user
     // Prevents overwriting of data with old data
     if (failedDownload) {
         await message.reply("Failed to connect to dropbox. Try again in a couple minutes");
+        return;
+    }
+
+    let author = message.author.id;
+
+    // Asks new users to use /update which handles new users
+    if (!leaderboard[author]) {
+        await message.reply("New accounts must set up their name using /update");
         return;
     }
 
@@ -146,65 +148,18 @@ client.on("messageCreate", async message => {
     console.log(matchResults);
 
     if (!matchResults) {
-        message.reply("Try updating your stats with the following format: " +
-            "D: # of demos E: # of exterminations Your Username\nEx: ```D: 2000 E: 1000 Demo Leaderboard```\n" +
-            "Or try the new /update command!");
+        message.reply("Invalid format, please update your stats with the following format: " +
+            "D: # of demos E: # of exterminations\nEx: ```D: 2000 E: 1000```\n" +
+            "Or use /update");
         return;
     }
 
     let demos = matchResults[1];
     let exterms = matchResults[2];
 
-    // Uses author Discord id to verify identity
-    // Discord gives every user an 18 digit identifier that cannot be duplicated
-    let author = message.author.id;
+    let name = leaderboard[author].Name;
 
-    // If there is a name mapped to the user's ID, updates that user's stats
-    // Otherwise warns user to use a name
-    // Should not be reached but ensures quality of data
-    if (!idmap[author]) {
-        // if the idmap doesn't include this discord ID and no name was given, returns and warns user
-        await message.reply("Account isn't mapped to a name, try using /update instead!");
-        return;
-    }
-
-    let name = idmap[author];
-
-    // changed JSON keeps track of if the JSON has been changed
-    let changedJSON = false;
-
-    // If the leaderboard doesn't include the name, adds it
-    if (!leaderboard[name]) {
-        leaderboard[name] = {Authorized: 0, Discord: "", Demos: 0, Exterminations: 0};
-        changedJSON = true;
-    }
-
-    // If the leaderboard doesn't have a discord ID attached, adds it
-    if (!leaderboard[name].Discord) {
-        leaderboard[name].Discord = author;
-        changedJSON = true;
-    }
-
-    // Ensures user can only change their score
-    // Warns user if the account is registered to another player
-    // should be unreachable but this is to make sure
-    if (leaderboard[name].Discord !== author) {
-        await message.reply("Cannot update leaderboard for other users, " +
-            "Please DM JerryTheBee if something is wrong");
-        return;
-    }
-
-    // If the ID map doesn't have a name attached, adds it
-    if (!idmap[author]) {
-        idmap[author] = name;
-        uploadIdMap(message);
-    }
-
-    if (changedJSON) {
-        uploadJSON(message);
-    }
-
-    addScores(leaderboard[name].Authorized, demos, exterms, name, author, message);
+    addScores(leaderboard[author].Authorized, demos, exterms, name, author, message);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -213,7 +168,6 @@ client.on('interactionCreate', async interaction => {
     // if the previous download failed, tries again
     if (failedDownload) {
         download();
-    
     }
 
     // if two in a row have failed, gives up and warns user
@@ -224,8 +178,6 @@ client.on('interactionCreate', async interaction => {
     }
 
     if (interaction.commandName === 'update') { 
-        interaction.reply("Leaderboard down for maintenance");
-        return;
         const demos = interaction.options.get('demolitions').value;
         const exterms = interaction.options.get('exterminations').value;
         let name = interaction.options.get('name')?.value;
@@ -234,73 +186,45 @@ client.on('interactionCreate', async interaction => {
 
         // if there is a name supplied
         if (name) {
-            // changed JSON keeps track of if the JSON has been changed
-            let changedJSON = false;
-    
-            // If the leaderboard doesn't include the name, adds it
-            if (!leaderboard[name]) {
-                leaderboard[name] = {Authorized: 0, Discord: "", Demos: 0, Exterminations: 0};
-                changedJSON = true;
-            }
+            // If the leaderboard doesn't include the author, adds them
+            // otherwise ignores name field
+            if (!leaderboard[author]) {
+                leaderboard[author] = {
+                    "Name": name,
+                    "Demolitions": 0,
+                    "Exterminations": 0,
+                    "LastUpdate": "2015-07-07T00:00:00.000",
+                    "Authorized": 0,
+                    "History": []
+                  };
+            } 
 
-            if (interaction.member.roles.cache.has(modRoleID)) {
-                leaderboard[name].Demos = demos;
-                leaderboard[name].Exterminations = exterms;
-                uploadFiles("\n\"" + name + "\"," + demos + "," + exterms, interaction);
-                await interaction.reply("Overriden the score of user " + name);
-                return;
-            }
+            // TODO temporarily disabled until reasonable solution is made for overrides
+            // if (interaction.member.roles.cache.has(modRoleID)) {
+            //     leaderboard[author].Demos = demos;
+            //     leaderboard[author].Exterminations = exterms;
+            //     leaderboard[author].name = name;
+            //     await interaction.reply("Overriden the score of user " + name);
+            //     return;
+            // }
     
-            // If the leaderboard doesn't have a discord ID attached, adds it
-            if (!leaderboard[name].Discord) {
-                leaderboard[name].Discord = author;
-                changedJSON = true;
-            }
-    
-            if (leaderboard[name].Discord !== author) {
-                await interaction.reply("That name is already taken, please try another");
-                return;
-            }
-    
-            // If the ID map doesn't have a name attached, adds it
-            if (!idmap[author]) {
-                idmap[author] = name;
-                uploadIdMap(interaction);
-            }
-    
-            if (changedJSON) {
-                uploadJSON(interaction);
-            }
+            // TODO: also solve duplicate names
+            // if (leaderboard[name].Discord !== author) {
+            //     await interaction.reply("That name is already taken, please try another");
+            //     return;
+            // }
+
         } else {
-            // if the idmap doesn't include this discord ID and no name was given, returns and warns user
-            if (!idmap[author]) {
-                await interaction.reply("Account isn't mapped to a name, try again including a name");
+            // if the leaderboard doesn't include this discord ID and no name was given, returns and warns user
+            if (!leaderboard[author]) {
+                await interaction.reply("New account has no name, try again including a name");
                 return;
             }
         }
 
-        // If there is a name mapped to the user's ID, updates that user's stats
-        // Otherwise warns user to use a name
-        // Should not be reached but ensures quality of data
-        if (idmap[author]) {
-            name = idmap[author];
-        } else {
-            await interaction.reply("Account isn't mapped to a name, try again including a name");
-            return;
-        }
+        name = leaderboard[author].Name
 
-        // Ensures user can only change their score
-        // Warns user if the account is registered to another player
-        // should be unreachable but this is to make sure
-        if (leaderboard[name].Discord !== author) {
-            interaction.channel.send("Cannot update leaderboard for other users, " +
-                "Please DM JerryTheBee if something is wrong");
-            return;
-        }
-
-        addScores(leaderboard[name].Authorized, demos, exterms, name, author, interaction);
-
-        //console.log(interaction.user.id);
+        addScores(leaderboard[author].Authorized, demos, exterms, name, author, interaction);
     }
 
     if (interaction.commandName === 'authorize') {
@@ -316,15 +240,14 @@ client.on('interactionCreate', async interaction => {
             return;
         }
 
-        // if the idmap doesn't include this discord ID and no name was given, returns and warns user
-        if (!idmap[user]) {
-            await interaction.reply({content:"User isn't mapped to a name", ephemeral: true});
+        // if the leaderboard doesn't include this discord ID, returns and warns user
+        if (!leaderboard[user]) {
+            await interaction.reply({content:"User <@" + user + 
+                "> isn't on the leaderboard. Have them use /update", ephemeral: true});
             return;
-        } 
+        }
 
-        //interaction.channel.send("mod " + author + " is authorizing user " + user + " aka " + name);
-
-        authorize(idmap[user], user, level, interaction);
+        authorize(user, level, interaction);
     }
 
     if (interaction.commandName === 'name') {
@@ -391,22 +314,6 @@ function download() {
             failedDownload = true;
             throw err;
         });
-    // Maps Discord IDs to names to prevent duplicates
-    dbx.filesDownload({path: "/idmap.json"})
-        .then(function (data) {
-            fs.writeFile("./idmap.json", data.fileBinary, 'binary', function (err) {
-                if (err) {
-                    failedDownload = true;
-                    throw err;
-                }
-                console.log('File: ' + data.name + ' saved.');
-                idmap = require("./idmap");
-            });
-        })
-        .catch(function (err) {
-            failedDownload = true;
-            throw err;
-        });
 
     dbx.filesDownload({path: "/highscores.json"})
         .then(function (data) {
@@ -429,52 +336,34 @@ function download() {
     }
 }
 
-function authorize(name, id, level, message) {
+function authorize(id, level, message) {
     // If the user isn't in the leaderboard, adds them
-    if (!leaderboard[name]) {
-        leaderboard[name] = {Authorized: 0, Discord: "", Demos: 0, Exterminations: 0};
+    if (!leaderboard[id]) {
+        console.log("Error updating invalid user " + id);
+        return;
     }
 
-    // Links ID to score and authorizes
-    leaderboard[name].Discord = id;
-    leaderboard[name].Authorized = level;
-
-    // Changes ID map to allow name changes of authorized users
-    idmap[id] = name;
-    uploadIdMap(message);
+    leaderboard[id].Authorized = level;
 
     // Uploads the updated JSON Leaderboard
     uploadJSON(message);
-    message.reply("Authorized " + name);
-    console.log("Authorized " + name + " at level " + level);
+    message.reply("Authorized " + leaderboard[id].Name);
+    console.log("Authorized " + leaderboard[id].Name + " at level " + level);
 }
 
 function nameUser(name, id, message) {
-    //console.log(name);
-
-    let oldAuth = 0;
-    if (idmap[id] && leaderboard[idmap[id]]) {
-        oldAuth = leaderboard[idmap[id]].Authorized;
+    // If the user isn't in the leaderboard, warns user
+    if (!leaderboard[id]) {
+        message.reply("User <@" + id + "> isn't in the leaderboard");
+        return;
     }
 
-    // If the user isn't in the leaderboard, adds them
-    if (!leaderboard[name]) {
-        leaderboard[name] = {Authorized: 0, Discord: "", Demos: 0, Exterminations: 0};
-    }
-
-    // Links ID to score and doesn't authorize
-    leaderboard[name].Discord = id;
-    leaderboard[name].Authorized = oldAuth;
-
-    // Changes ID mapping of user to give a new name
-    idmap[id] = name;
-    uploadIdMap(message);
+    // Links ID to name
+    leaderboard[id].Name = name;
 
     // Uploads the updated JSON Leaderboard
     uploadJSON(message);
-    message.reply("Renamed " + name);
-    //console.log("Renamed " + name);
-    //console.log(leaderboard[name]);
+    message.reply("Renamed <@" + id + " to " + name);
 }
 
 async function addScores(authorized, demos, exterms, name, id, interaction) {
@@ -526,8 +415,16 @@ async function addScores(authorized, demos, exterms, name, id, interaction) {
         uploadHighScores();
     }
 
-    leaderboard[name].Demos = demos;
-    leaderboard[name].Exterminations = exterms;
+    leaderboard[id].Demos = demos;
+    leaderboard[id].Exterminations = exterms;
+    let currTime = new Date();
+    let currTimeString = currTime.toISOString();
+    leaderboard[id].LastUpdate = currTimeString;
+    leaderboard[id].History.push({
+        "Demolitions": demos,
+        "Exterminations": exterms,
+        "Time": currTimeString
+    });
     uploadFiles("\n\"" + name + "\"," + demos + "," + exterms, interaction);
     await interaction.reply("<@" + id + "> has " + demos + " demos and " + exterms + " exterms");
 }
@@ -567,18 +464,6 @@ async function writeCSV(message, content) {
     });
 }
 
-// uploads the ID mapping JSON file to Dropbox
-function uploadIdMap(message) {
-    dbx.filesUpload({path: '/idmap.json', contents: JSON.stringify(idmap), mode: "overwrite"})
-        .catch(function (error) {
-            message.channel.send("Dropbox error for id map. Try same command again");
-            console.error(error);
-        });
-
-    message.channel.send("Updated ID Map");
-    console.log("Uploaded idmap JSON");
-}
-
 // uploads the JSON file leaderboard to Dropbox
 function uploadJSON(message) {
     dbx.filesUpload({path: '/leaderboard.json', contents: JSON.stringify(leaderboard), mode: "overwrite"})
@@ -590,7 +475,7 @@ function uploadJSON(message) {
     console.log("Uploaded leaderboard JSON");
 }
 
-// uploads the JSON file leaderboard to Dropbox
+// uploads the JSON file high scores to Dropbox
 function uploadHighScores(message) {
     dbx.filesUpload({path: '/highscores.json', contents: JSON.stringify(highscores), mode: "overwrite"})
         .catch(function (error) {
